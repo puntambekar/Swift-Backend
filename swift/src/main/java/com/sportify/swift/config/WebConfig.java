d2eb4b48c2f4b569b55c099ec17143dbe369c39a
Bhagyashree <p.bhagyashree992@gmail.com>
2024-05-19 16:52:21 -0400

    removed localhost references for deployment, initial code

---
+++ swift/src/main/java/com/sportify/swift/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.sportify.swift.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${app.allowed.origins}")
+    private String theAllowedorigins;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(theAllowedorigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
