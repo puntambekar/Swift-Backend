aed0f3aa2232e64aa3c4c584ffcf171ab90c5ba0
Bhagyashree <p.bhagyashree992@gmail.com>
2024-05-19 16:56:17 -0400

    changed directory structure

--- swift/src/main/java/com/sportify/swift/dao/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sportify.swift.dao;
-
-import com.sportify.swift.entity.Booking;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "booking", path = "booking")
-public interface BookingRepository extends MongoRepository<Booking,String> {
-}
